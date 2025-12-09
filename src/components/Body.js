4bab5d0a31dd2a2a4bc7e071cecde4fd251bc198
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2025-12-09 23:29:06 +0530

    cors plogin issue resolved

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,16 +18,21 @@ const Body = () => {
 
   const fetchedData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://api.allorigins.win/raw?url=" +
+        encodeURIComponent(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        )
     );
+
     const json = await data.json();
+    console.log(json);
 
     setListOfResturant(
-      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
 
     setFilteredResturant(
-      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
